802e1108f519288d125a963f0dc4e78f17fd9699
Virendrasinh Khuman <virendrasinhkhuman@gmail.com>
2023-02-17 10:07:10 +0530

    minor changes in scheduling algorithm and gantt chart

---
+++ dfhdakfj.js
@@ -0,0 +1,230 @@
+// reffrence to first table
+let table1 = document.getElementById("table1");
+// reffrence to the second table
+let table2 = document.getElementById("table2");
+//current data array
+let data = [0,0,0];
+let entries=0;
+//raumdueeter
+//creating an identifier for table 1 body
+const t1=document.querySelector('#table1');
+let pid=[];
+//------------------------------------------------------------------
+function addrow(){
+    //loading the current data(user input) into current data array
+    data[0] = document.getElementById("PID").value;
+    data[1] = document.getElementById("AT").value;
+    data[2] = document.getElementById("BT").value;
+    if(data[0]==""|| data[1]=="" || data[2]==""){
+        alert("No field can empty");
+        return;
+    }
+    console.log(data[0]);
+    if(data[1]<0){
+        alert("Arrival time cannot be negative");
+        return;
+    }
+    if(data[2]<0){
+        alert("Burst time cannot be negative");
+        return;
+    }
+    if((pid.includes(data[0]))){
+        alert("Process id must be unique");
+        return;
+    }
+    document.getElementById("tablebuttons").style.scale="100%";
+    table1.style.scale="100%";
+    
+    //creating table row in the html page using the below code
+    const tr=document.createElement('tr');
+    const td1=document.createElement('td');
+    const td2=document.createElement('td');
+    const td3=document.createElement('td');
+    //putting the data collected from user into this table
+    td1.textContent=data[0];
+    td2.textContent=data[1];
+    td3.textContent=data[2];
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    //appending the row into the table body
+    t1.appendChild(tr);
+    document.getElementById("tablebuttons").style.transform="scaleY(100%)";
+    table1.style.transform="scaleY(100%)";
+    //extra code
+    //creating a new row
+    // var row = table1.insertRow();
+    // //loop to insert cells
+    // for(var i=0;i<3;i++){
+    //     //creating and adding cell
+    //     var cell = row.insertCell(i);
+    //     //giving data to cell
+    //     cell.innerHTML = data[i];
+    // }
+    entries+=1;
+    pid.push(data[0]);
+}
+function resettable(){
+    for(let k=0;k<entries;k++){
+        console.log(entries);
+        document.getElementById("table1").deleteRow(1);
+    }
+    document.getElementById("tablebuttons").style.transform="scaleY(0%)";
+    table1.style.transform="scaleY(0%)";
+    pid=[];
+    completed=[];
+    entries=0;
+}
+function data_extractor(){
+    let inputarr=[]
+    for(let i=1;i<=entries;i++){
+        let a1=parseInt(document.getElementById("table1").rows[i].cells[0].innerHTML);
+        let a2=parseInt(document.getElementById("table1").rows[i].cells[1].innerHTML);
+        let a3=parseInt(document.getElementById("table1").rows[i].cells[2].innerHTML);
+        let arr=[a1,a2,a3];
+        inputarr.push(arr);
+    }
+    console.log("This is the input array");
+    console.log(inputarr);
+    scheduler(inputarr);
+}
+let time=0;
+let completed=[];
+//this function would do the scheduling and get the completion time stored in an array which would be returned
+function scheduler(inputarray){
+    input=inputarray;
+    // let input=inputarray;
+    let processQueue=[];//complete input
+    let readyQueue=[];
+    // completed=[];
+    // time=0;
+    let no_of_processes=entries;
+    while(no_of_processes>0){
+        let in_r=0;
+        let c=0;
+        for(let i=0;i<input.length;i++){
+            console.log("input is : "+input);
+            if(time>=input[i][1] && !(readyQueue.includes(input[i]))){
+                let temp=[];
+                temp.push(input[i][0]);
+                temp.push(input[i][1]);
+                temp.push(input[i][2]);
+                readyQueue.push(temp);
+                in_r+=1;
+                console.log("rq:"+readyQueue);
+            }
+        }
+        if(in_r>0)
+        {
+            let min=1000;
+            for(let j=0;j<readyQueue.length;j++){
+                if(readyQueue[j][2]<min && readyQueue[j].length==3){
+                    min=readyQueue[j][2];
+                }
+            }
+            console.log("min: "+min);
+
+            for(let j=0;j<readyQueue.length;j++){
+                if(readyQueue[j][2]==min){
+                    time+=readyQueue[j][2];
+                    let temp=readyQueue[j];
+                    temp.push(time);
+                    temp.push((time-temp[1]));
+                    temp.push((temp[4]-temp[2]));
+                    completed.push(temp);
+                    console.log(readyQueue[j]);
+                    let ind=input.indexOf(readyQueue[j]);
+                    console.log("ind is: "+ind);
+                    input=input.splice(ind,1);
+                    readyQueue=readyQueue.splice(j,1);
+                    console.log("ready queue: "+readyQueue);
+                    in_r--;
+                    no_of_processes--;
+                    // readyQueue[j].push(time);
+                    c=1;
+                    break;
+                }
+        }
+        min=100;
+        }
+        if(c==0){
+            time+=1;
+        }       
+    }
+    let ret=readyQueue;
+    console.log("output");
+    console.log(completed);
+    filltable(completed);
+    return ret;    
+}
+function addroww(dt){
+    //creating a new row
+    var row = table1.insertRow();
+    //loop to insert cells
+    for(var i=0;i<6;i++){
+        //creating and adding cell
+        var cell = row.insertCell();
+        //giving data to cell
+        cell.innerHTML = dt[i];
+    }
+}
+function filltable(){
+    console.log("length of completed is :"+completed.length);
+    for(let i=0;i<completed.length;i++){
+        document.getElementById("table1").deleteRow(1);
+    }
+    // resettable();
+    document.getElementById("tablebuttons").style.transform="scaleY(100%)";
+    table1.style.transform="scaleY(100%)";
+    console.log("completed length: ",completed.length);
+    for(let i=0;i<completed.length;i++){
+        addroww(completed[i]);
+    }
+    create();
+    completed=[];
+    time=0
+}
+///
+let box = document.getElementById("chart"); //refferne to the outer box
+let divarray =[]; //array of all the div(smaller boxes) which will be added to the outer box
+let i = -1;//index of the smaller boxes(divarray)
+let dataa=[];
+// for(let w=0;w<completed.length;w++){
+//     dataa.push(completed[2]);
+// }
+let timer=0;//timer to print current time at the corner
+
+let text; //append the current time to the smaler boxes
+let col = ["red","#b30047"]; //random color cboose
+//calling create function
+function create(){
+    i=-1;
+    for(let j=0;j<completed.length;j++){
+        createDiv(); //creating smaller boxes equalent to the no. of elements in the burst time array
+    }
+    resizeDiv();//resizeing them
+}
+function createDiv(){
+    let totaltime = time-1;
+    i=i+1; //inc divarray index
+    divarray[i]=document.createElement("div"); //create smaller box and add to divarray
+    box.appendChild(divarray[i]); //appending to the main box
+    text = document.createElement("span"); //creating text
+    // timer = timer+ completed[i][2]; //setting the current time
+    console.log("completed is: ",completed);
+    text.textContent = "P"+completed[i][0]+"("+(completed[i][3]-completed[i][2])+" - "+completed[i][3]+")";
+    text.style.paddingTop = "50px"; //padding in order to put them at bottom of box
+    divarray[i].appendChild(text); //appending to the smaller box
+    divarray[i].style.display="flex"; //usng flex to automatically move the text
+    divarray[i].style.justifyContent="center"; //moving the text to the right most corner
+    divarray[i].style.backgroundColor = col[i%2]; //setting color
+    divarray[i].style.border = "0.2px solid white"; //setting border
+}
+function resizeDiv(){
+    let totaltime = time-1;
+    for(let j=0;j<divarray.length;j++){
+        divarray[j].style.width = ((completed[j][2]/totaltime)*100)+"%";
+        console.log(totaltime);
+        console.log(((completed[j][2]/totaltime)*100)+"%"); //setting width
+    }
+}
--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-// reffrence to first table
-let table1 = document.getElementById("table1");
-// reffrence to the second table
-let table2 = document.getElementById("table2");
 //current data array
 let data = [0,0,0];
-let entries=0;
 //raumdueeter
 //creating an identifier for table 1 body
 const t1=document.querySelector('#table1');
 let pid=[];
+let completed=[];
+let entries=0; //process counter
 //------------------------------------------------------------------
 function addrow(){
     //loading the current data(user input) into current data array
     data[0] = document.getElementById("PID").value;
     data[1] = document.getElementById("AT").value;
     data[2] = document.getElementById("BT").value;
+    //------------------------------------------------------------------
+    //input checker
     if(data[0]==""|| data[1]=="" || data[2]==""){
         alert("No field can empty");
         return;
@@ -32,8 +31,10 @@ function addrow(){
         alert("Process id must be unique");
         return;
     }
-    document.getElementById("tablebuttons").style.scale="100%";
-    table1.style.scale="100%";
+    //------------------------------------------------------------------
+    //animating the table into the html
+    document.getElementById("tablebuttons").style.transform="scale(100%)";
+    table1.style.transform="scale(100%)";
     //creating table row in the html page using the below code
     const tr=document.createElement('tr');
     const td1=document.createElement('td');
@@ -62,6 +63,7 @@ function addrow(){
     entries+=1;
     pid.push(data[0]);
 }
+//------------------------------------------------------------------
 function resettable(){
     for(let k=0;k<entries;k++){
         console.log(entries);
@@ -72,6 +74,7 @@ function resettable(){
     pid=[];
     completed=[];
     entries=0;
+    time=0;
 }
 function data_extractor(){
     let inputarr=[]
@@ -87,55 +90,100 @@ function data_extractor(){
     scheduler(inputarr);
 }
 let time=0;
-let completed=[];
+let free=[];
 //this function would do the scheduling and get the completion time stored in an array which would be returned
 function scheduler(input){
-    let processQueue=[];//complete input
-    let readyQueue=[];
-    let no_of_processes=entries;
-    while(no_of_processes>0){
-        let in_r=0;
-        let c=0;
-        for(let i=0;i<input.length;i++){
-            if(time>=input[i][1] && !(readyQueue.includes(input[i]))){
-                readyQueue.push(input[i]);
-                in_r+=1;
-            }
-        }
-        if(in_r>0)
-        {
-            let min=1000;
-            for(let j=0;j<readyQueue.length;j++){
-                if(readyQueue[j][2]<min && readyQueue[j].length==3){
-                    min=readyQueue[j][2];
-                }
-            }
-            for(let j=0;j<readyQueue.length;j++){
-                if(readyQueue[j][2]==min){
-                    time+=readyQueue[j][2];
-                    let temp=readyQueue[j];
-                    temp.push(time);
-                    temp.push((time-temp[1]));
-                    temp.push((temp[4]-temp[2]));
-                    completed.push(temp);
-                    readyQueue.splice(j,1);
-                    in_r--;
-                    no_of_processes--;
-                    // readyQueue[j].push(time);
-                    c=1;
+    pid=[];
+    at=[];
+    bt=[];
+    flag=[];
+    for(let w=0;w<input.length;w++){
+        pid.push(input[w][0]);
+        at.push(input[w][1]);
+        bt.push(input[w][2]);
+        flag.push(0);
+    }    
+    var n = pid.length;
+            var clock = 0;
+            var tot = 0;
+            var items =[];
+            var ct=[];
+            var ta=[];
+            var wt=[];
+            var avgwt=0;
+            var avgta=0;
+            
+
+            while(true)
+            {
+                var min=100;
+                var c = n; // c represents the current PID
+                if (tot == n) // total no of process = completed process loop will be terminated
                     break;
+                
+                for (var i=0; i< n; i++)
+                {
+                    /*
+                     * If i'th process arrival time <= system time and its flag=0 and burst<min 
+                     * That process will be executed first 
+                     */ 
+                    var count=0;
+                    if ((at[i] <= clock) && (flag[i] == 0) && (bt[i]<min))
+                        {
+                            min=bt[i];
+                            c=i;
+                        } 
+
                 }
-        }
-        }
-        if(c==0){
-            time+=1;
-        }       
-    }
-    let ret=readyQueue;
-    console.log("output");
-    console.log(completed);
-    filltable(completed);
-    return ret;    
+                /* If c==n means c value can not updated because no process arrival time< system time so we increase the system time */
+                if (c==n){
+                    let a;
+                    a=clock;
+                    let b=[];
+                    b.push(a);
+                    b.push(a+1);
+                    clock++;
+                    free.push(b);
+                } 
+                    
+                else
+                {
+                    var temp = [];
+                    temp.push(pid[c]);
+                    temp.push(bt[c]);
+                    items.push(temp);
+
+                    ct[c]=clock+bt[c];
+                    ta[c]=ct[c]-at[c];
+                    wt[c]=ta[c]-bt[c];
+                    
+                    clock+=bt[c];
+                    flag[c]=1;
+                    tot++;   
+                }
+            }
+
+            for(i=0;i<n;i++)
+            {
+                avgwt +=wt[i];
+                avgta +=ta[i];
+            }
+
+            avgwt/=n;
+            avgta/=n;
+            for(let e=0;e<ct.length;e++){
+                let temp=[];
+                temp.push(pid[e]);
+                temp.push(at[e]);
+                temp.push(bt[e]);
+                temp.push(ct[e]);
+                temp.push(ta[e]);
+                temp.push(wt[e]);
+                completed.push(temp);
+            }
+            filltable();
+            // printStat(ct,ta,wt,avgwt,avgta,pid); 
+            // return items;
 }
 function addroww(dt){
     //creating a new row
@@ -178,31 +226,80 @@ let text; //append the current time to the smaler boxes
 let col = ["red","#b30047"]; //random color cboose
 //calling create function
 function create(){
-    for(let j=0;j<completed.length;j++){
+    i=-1;
+    for(let j=0;j<(completed.length + free.length);j++){
         createDiv(); //creating smaller boxes equalent to the no. of elements in the burst time array
     }
-    resizeDiv();//resizeing them
+    // resizeDiv();//resizeing them
 }
+let lst1=[];
+let lst2=[];
 function createDiv(){
     let totaltime = time-1;
     i=i+1; //inc divarray index
-    divarray[i]=document.createElement("div"); //create smaller box and add to divarray
-    box.appendChild(divarray[i]); //appending to the main box
-    text = document.createElement("span"); //creating text
-    // timer = timer+ completed[i][2]; //setting the current time
-    text.textContent = "P"+completed[i][0]+"("+(completed[i][3]-completed[i][2])+" - "+completed[i][3]+")";
-    text.style.paddingTop = "50px"; //padding in order to put them at bottom of box
-    divarray[i].appendChild(text); //appending to the smaller box
-    divarray[i].style.display="flex"; //usng flex to automatically move the text
-    divarray[i].style.justifyContent="center"; //moving the text to the right most corner
-    divarray[i].style.backgroundColor = col[i%2]; //setting color
-    divarray[i].style.border = "0.2px solid white"; //setting border
-}
-function resizeDiv(){
-    let totaltime = time-1;
-    for(let j=0;j<divarray.length;j++){
-        divarray[j].style.width = ((completed[j][2]/totaltime)*100)+"%";
-        console.log(totaltime);
-        console.log(((completed[j][2]/totaltime)*100)+"%"); //setting width
+    let d=0;
+    let count=0;
+    console.log(completed[2]);
+    for(let w=0;w<free.length;w++){
+        d=0;
+        if(free[w][0]==i){
+            d=1;
+            count++;
+            lst1.push(i);
+            divarray[i]=document.createElement("div");
+            box.appendChild(divarray[i]);
+            text = document.createElement("span");
+            text.textContent = (free[w][0])+" - "+free[w][1];
+            text.style.paddingTop = "50px"; //padding in order to put them at bottom of box
+            divarray[i].appendChild(text); //appending to the smaller box
+            divarray[i].style.display="flex"; //usng flex to automatically move the text
+            divarray[i].style.justifyContent="center"; //moving the text to the right most corner
+            divarray[i].style.backgroundColor = "white"; //setting color
+            divarray[i].style.border = "0.2px solid white";
+            divarray[i].style.width=(1+"px"); 
+            divarray[i].style.flexGrow=1;
+            break;
+        }
     }
+    
+        if(d==0){
+            lst2.push(i);
+            divarray[i]=document.createElement("div"); //create smaller box and add to divarray
+            box.appendChild(divarray[i]); //appending to the main box
+            text = document.createElement("span"); //creating text
+            // timer = timer+ completed[i][2]; //setting the current time
+            text.textContent = "P"+completed[i-count-1][0]+"("+(completed[i-count-1][3]-completed[i-count-1][2])+" - "+completed[i-count-1][3]+")";
+            
+            text.style.paddingTop = "50px"; //padding in order to put them at bottom of box
+            divarray[i].appendChild(text); //appending to the smaller box
+            divarray[i].style.display="flex"; //usng flex to automatically move the text
+            divarray[i].style.justifyContent="center"; //moving the text to the right most corner
+            divarray[i].style.backgroundColor = col[i%2]; //setting color
+            divarray[i].style.border = "0.2px solid white"; //setting border
+            // console.log(completed.length+"    "+(i-count));
+            console.log("bt"+completed[i-count-1][2]);
+            divarray[i].style.width=completed[i-count-1][2]+"px";
+            divarray[i].style.flexGrow=completed[i-count-1][2];
+            console.log(divarray[i].style.width);
+            }
+        
+    
 }
+// function resizeDiv(){
+//     let totaltime = time-1;
+//     let r=0;
+//     for(let j=0;j<divarray.length;j++){
+    
+//         if(divarray[i].style.backgroundColor =="white"){
+//             r+=1;
+//             divarray[j].style.width=((1/totaltime)*100)+"%";
+//         }
+//         else{
+//             console.log("r is:"+r);
+//             divarray[j].style.width = ((completed[j-r][2]/totaltime)*100)+"%";
+//         }
+        
+//         console.log(totaltime);
+//         // console.log(((completed[j][2]/totaltime)*100)+"%"); //setting width
+//     }
+// }
